33958e950baa62849b6faa5d53adcbe79b27d771
navneet7393 <56058754+navneet7393@users.noreply.github.com>
2020-04-29 21:51:13 -0700

    Ready

---
+++ inc/information.inc.php
@@ -0,0 +1,35 @@
+<div class="container text-white bg-info">
+<h3><strong>Information</strong></h3>
+<div class="row">
+<div class=" col-xs-12 col-sm-12 col-md-4 col-lg-6">
+	<h5>Celsius to Fahrenheit</h5>
+	<p> Formula: (0°C × 9/5) + 32 = 32°F</p>
+</div>
+
+<div class="col-xs-12 col-sm-12 col-md-4 col-lg-6">
+	<h5>Celsius to Kelvin</h5>
+	<p> Formula: 0°C + 273.15 = 273.15K</p>
+</div>
+
+<div class="col-xs-12 col-sm-12 col-md-4 col-lg-6">
+	<h5>Fahrenheit to Celsius</h5>
+	<p> Formula: (32°F − 32) × 5/9 = 0°C</p>
+</div>
+
+<div class="col-xs-12 col-sm-12 col-md-4 col-lg-6">
+	<h5>Fahrenheit to Kelvin</h5>
+	<p>Formula: (32°F − 32) × 5/9 + 273.15 = 273.15K</p>
+</div>
+
+<div class="col-xs-12 col-sm-12 col-md-4 col-lg-6">
+	<h5>Kelvin to Celsius</h5>
+	<p>Formula: 0K − 273.15 = -273.1°C</p>
+</div>
+
+<div class="col-xs-12 col-sm-12 col-md-4 col-lg-6">
+	<h5>Kelvin to Fahrenheit</h5>
+	<p>Formula: (0K − 273.15) × 9/5 + 32 = -459.7°F</p>
+</div>
+<br> <br>
+<!-- end of row -->
+</div> 
